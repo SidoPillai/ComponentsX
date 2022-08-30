6fdb650b6e0b6244a5996a0a4c09b499b005c925
Siddesh Pillai <siddeshvijji@gmail.com>
2022-08-30 16:38:55 -0700

    Added Neomorphic modifier

---
+++ components/src/main/java/com/compose/modifiers/shapes/Punched.kt
@@ -0,0 +1,28 @@
+package com.compose.modifiers.shapes
+
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.compose.modifiers.internal.BlurMaker
+
+/**
+ * Punched or Bumped shape
+ *    _________
+ * __/         \__
+ */
+open class Punched(private val cornerType: CornerType = CornerType.Rounded()) : NeuShape {
+
+    override fun drawShadows(
+        drawScope: ContentDrawScope,
+        blurMaker: BlurMaker,
+        shapeConfig: ShapeConfig
+    ) {
+        shapeConfig.cornerType = cornerType
+        drawScope.drawOnBackground(shapeConfig, blurMaker)
+        drawScope.drawContent()
+    }
+
+    class Oval() : Punched(CornerType.Oval)
+
+    class Rounded(radius: Dp = 12.dp) : Punched(CornerType.Rounded(radius))
+}
